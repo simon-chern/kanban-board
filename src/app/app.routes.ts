63717194f2134e56d5e4004dd1c9d266f31b4a4d
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-04-09 20:23:36 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { BoardComponent } from './board/board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: LandingComponent},
+    { path: "board", component: BoardComponent}
+];
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [MatButtonModule, RouterLink],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+
+}
