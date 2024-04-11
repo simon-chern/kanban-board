30cb81d2f82bf114cf22e7ff0b37b280db3d034b
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-04-11 20:01:58 +0300

    theme toggle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kanban';
-  
+  class: string = '';
+  isDark = false;
+  toggleTheme(event: any) {
+    if (event.checked) {
+      this.class = 'theme-dark';
+    } else {
+      this.class = '';
+    }
+  }
 }
