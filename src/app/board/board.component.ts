9a099af1ebd8ae7df1e727fe10162714713c593a
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-04-12 21:57:49 +0300

    added drag and drop

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { Task } from './task';
 import { TaskComponent } from "../task/task.component";
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, DragDropModule, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-board',
     standalone: true,
     templateUrl: './board.component.html',
     styleUrl: './board.component.scss',
-    imports: [TaskComponent, DragDropModule]
+    imports: [TaskComponent, DragDropModule, MatButtonModule, MatIconModule]
 })
 export class BoardComponent {
-  tasks: Task[] = [
+  done: Task[] = [];
+  inProgress: Task[] = [];
+  todo: Task[] = [
     {
       title: 'Buy milk',
       description: 'Go to the store and buy milk'
@@ -21,4 +25,20 @@ export class BoardComponent {
       description: 'Using Firebase and Angular create a Kanban app!'
     }
   ];
+  editTask(list: string, task: Task): void {};
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
