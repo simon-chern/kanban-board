e9f638fde430752ee703997c6f5c48551f59e7aa
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-04-18 20:48:47 +0300

    made registration of a user

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import { Observable, from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private fireBaseAuth: Auth) { }
+  register(email: string, username: string, password: string): Observable<void> {
+    const promise = createUserWithEmailAndPassword(this.fireBaseAuth, email, password)
+      .then(response => updateProfile(response.user, { displayName: username}));
+    return from(promise);
+  }
+}
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField } from '@angular/material/form-field';
+
+import { AuthService } from '../auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { __values } from 'tslib';
+import { Router } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormField, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIcon],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  hide = true;
+  userForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  errorMessage = 'Enter correct value';
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+  Submit(): void {
+    const values = this.userForm.getRawValue();
+    const email = values.email!;
+    const username = values.username!;
+    const password = values.password!;
+    this.authService.register(email, username, password)
+    .subscribe(() => {
+      this.router.navigateByUrl('/board');
+    })
+  }
+}
+
