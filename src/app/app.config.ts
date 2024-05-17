602691b8a9a0707dda45ffad7f3768492b958664
simon-chern <148482820+simon-chern@users.noreply.github.com>
2024-05-17 19:51:37 +0300

    env variable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { apiKey } from './apikey';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"kanban-7f8a0","appId":"1:690259389221:web:cc48e64fdf09fff24dc8cc","storageBucket":"kanban-7f8a0.appspot.com","apiKey":"AIzaSyBfyjxUxYLhMJKZy8GP9W_Yl87ZxQtBVvY","authDomain":"kanban-7f8a0.firebaseapp.com","messagingSenderId":"690259389221","measurementId":"G-0CS6E2XBBV"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
+  providers: [
+    provideRouter(routes), provideAnimationsAsync(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(apiKey))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage()))]
 };
